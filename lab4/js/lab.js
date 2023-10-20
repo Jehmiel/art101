252c83a993f854819ed0d903bf6f4d548a9c17b9
Jehmiel <jsvinlua@ucsc.edu>
2023-10-20 15:47:20 -0700

    lab4changes

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,27 @@
+
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Jehmiel Vinluan 
+// Date: October 
+
+//Pseudocode of Rock Paper Scissors
+//Accept input from player 1 for rock, paper, or scissors
+//Start countdown timer 
+  //When the countdown timer ends, both players’ moves are revealed
+//Choose random number between 1 and 3
+  //If number is one, then return rock
+  //If number is two, then return paper
+  //If number is three, then return scissors
+//Test for win/lose condition
+  //If player 1 has rock, and number is 1, then it is a tie
+  //If player 1 has rock and number is 2, then player 1 loses
+  //If player 1 has rock and number is 2, then player 1 wins
+  //If player 1 has paper and number is 1, then player 1 wins
+  //If player 1 has paper and number is 2, then it is a tie
+  //If player 1 has paper and number is 3, then player 1 loses
+  //If player 1 has scissors and number is 1, then player 1 loses
+  //If player 1 has scissors and number is 2, then player 1 wins
+  //If player 1 has scissors and number is 3, then it is a tie
+//After the round program restarts with a loop to continue playing.
 
 // Constants
 
