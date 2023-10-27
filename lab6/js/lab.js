77487714242587c67356ad89d55ef8dc212c869b
Jehmiel <jsvinlua@ucsc.edu>
2023-10-27 15:43:27 -0700

    lab6changes

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,30 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Experiment with JavaScript arrays and objects.
+// Author: Jehmiel Vinluan
+// Date: 27 October 2023
+
+// Array for myTransport
+var myTransport = ["legs", " bus", " bike", " airplane", " car"]
+
+// Object for myMainRide 
+var myMainRide = {
+  make: "Honda",
+  model: "HR-V",
+  color: "gray",
+  year: "2024",
+  age: function() {
+      return 2023 - this.year;
+  }
+}
+// Output
+document.writeln("Some of the types of transportation I use are: ", myTransport, "</br>");
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
+
+
+
+
 
 // Constants
 
